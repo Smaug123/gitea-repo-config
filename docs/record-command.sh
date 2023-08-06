bb88cb256c2fc6cfcde640073b1c25381db9c85b
Smaug123 <patrick+github@patrickstevens.co.uk>
2023-08-06 22:26:51 +0100

    Better

---
+++ docs/record-command.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+export GITEA_TOKEN="$1"
+
+function execute() {
+    target="$1"
+    asciinema rec --overwrite --cols 100 --env "GITEA_TOKEN,SHELL,TERM" --command 'nix run github:Smaug123/dotnet-gitea-declarative -- \
+    --config-file /tmp/GiteaConfig.json \
+    --gitea-host=https://gitea.patrickstevens.co.uk \
+    --gitea-admin-api-token "$GITEA_TOKEN" \
+&& echo -n ">" && sleep 1 && echo -n " "' "$target.cast"
+
+    cat "$target.cast" | ../node_modules/.bin/svg-term > "$target.svg"
+}
+
+execute "after-deletion"
