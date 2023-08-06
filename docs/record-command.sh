6562784d24526655bfcf36d0c4134fd17d9732d4
Smaug123 <patrick+github@patrickstevens.co.uk>
2023-08-06 22:35:47 +0100

    Better

--- docs/record-command.sh
+++ docs/record-command.sh
@@ -8,9 +8,9 @@ function execute() {
     --config-file /tmp/GiteaConfig.json \
     --gitea-host=https://gitea.patrickstevens.co.uk \
     --gitea-admin-api-token "$GITEA_TOKEN" \
-&& echo -n ">" && sleep 1 && echo -n " "' "$target.cast"
+; echo -n ">" && sleep 1 && echo "\n> "' "$target.cast"
 
     cat "$target.cast" | ../node_modules/.bin/svg-term > "$target.svg"
 }
 
-execute "after-deletion"
+execute "no-op"
