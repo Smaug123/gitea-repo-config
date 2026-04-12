773484df2e70ba7d3d20ebb2e509476854396ce7
Patrick Stevens <3138005+Smaug123@users.noreply.github.com>
2026-04-12 10:32:28 +0100

    Switch to Lychee (#191)

--- flake.nix
+++ flake.nix
@@ -67,7 +67,7 @@
           ];
           packages = [
             pkgs.alejandra
-            pkgs.nodePackages.markdown-link-check
+            pkgs.lychee
             pkgs.shellcheck
           ];
         };
